d00c51fd1ad6e1690102fe1e4bcc425a10557a82
John Çamkıran <41493682+johncamkiran@users.noreply.github.com>
2022-12-02 10:20:42 +0300

    Preallocate cell array for speed

--- extracopularity.m
+++ extracopularity.m
@@ -55,21 +55,16 @@ function [E, k, m, I, S] = extracopularity(varargin)
 
 %{
 Copyright (c) 2022 John CAMKIRAN
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
 1. Redistributions of source code must retain the above copyright notice,
    this list of conditions and the following disclaimer.
-
 2. Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
-
 3. Neither the name of the copyright holder nor the names of its
    contributors may be used to endorse or promote products derived from
    this software without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -175,6 +170,7 @@ numTimesteps = numel(S);
 % Initialize output cell arrays
 E = cell(1,numTimesteps);
 k = cell(1,numTimesteps);
+m = cell(1,numTimesteps);
 
 % Precompute pair indices up to 14
 pairs = nchoosek(1:14,2);
